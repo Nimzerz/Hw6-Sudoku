09bc1ccd6a27fd07a86ef32db00b00676853a1fe
Nimzerz <aminaimi14@gmail.com>
2019-12-01 16:34:17 -0800

    unfinished code, passes all tests

--- src/sudoku.h
+++ src/sudoku.h
@@ -57,8 +57,58 @@ public:
         }
     }
 
+    // returns true if the given value is valid for the given row and column in the grid,
+    // otherwise returns false
+    bool valid(int val, int row, int column) {
+        // checks if location already is assigned
+        if (grid[row][column] != 0) {
+            return false;
+        }
+
+        // checks if value appears in the same row or column
+        for (int i = 0; i < 9; i++) {
+            if (grid[row][i] == val || grid[i][column] == val) {
+                return false;
+            }
+        }
+
+        // checks if value appears in the same box
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (grid[i + (row - row % 3)][j + (column - column % 3)] == val) {
+                    return false;}
+            }
+        }
+        return true;
+    }
+
+    bool findUnassignedLocation(int &row, int &column) {
+        for (row = 0; row < 9; row++) {
+            for (column = 0; column < 9; column++) {
+                if (grid[row][column] == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     bool solve() {
         // homework
-        return false;   // placeholder
+        int row;
+        int column;
+        if (!findUnassignedLocation(row, column)) {
+            return true;
+        }
+        for (int num = 1; num <= 9; num++) {
+            if (valid(num, row, column)) {
+                grid[row][column] = num;
+                if (solve()) {
+                    return true;
+                }
+                grid[row][column] = 0;
+            }
+        }
+        return false;   // backtrack and try another value
     }
 };
